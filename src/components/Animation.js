ffe29816b0240ae369de9c42e7d02c0473383bd6
AliKamal <alikamal2381@gmail.com>
2020-07-23 00:58:31 +0500

    second-commit

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,6 +1,19 @@
-import React, { useEffect } from "react";
+import React, { /* useState */ useEffect } from "react";
+import useWebAnimations from "@wellyshen/use-web-animations";
 
 function Animation() {
+  //const [duration, setDuration] = useState(0);
+
+  /*   const { ref, playState, getAnimations } = useWebAnimations({
+    keyframes: [
+      { transform: "translate(0px)" },
+      { transform: "translate(500px)" },
+    ],
+    timing: {
+      duration: 3000,
+    },
+  }); */
+
   useEffect(() => {
     console.log("we called useEffect");
     /* Background animations */
@@ -26,6 +39,7 @@ function Animation() {
       sceneryFrames,
       sceneryTimingBackground
     );
+
     background1Movement.currentTime =
       background1Movement.effect.timing.duration / 2;
 
@@ -116,79 +130,79 @@ function Animation() {
   return (
     <div>
       <h1>Lets Animation Start</h1>
-      <div class="wrapper">
-        <div class="sky"></div>
-        <div class="earth">
+      <div className="wrapper">
+        <div className="sky"></div>
+        <div className="earth">
           <div id="red-queen_and_alice">
             <img
               id="red-queen_and_alice_sprite"
               src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"
-              srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x"
+              srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x"
               alt="Alice and the Red Queen running to stay in place."
             />
           </div>
         </div>
 
-        <div class="scenery" id="foreground1">
+        <div className="scenery" id="foreground1">
           <img
             id="palm3"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x"
             alt=" "
           />
         </div>
-        <div class="scenery" id="foreground2">
+        <div className="scenery" id="foreground2">
           <img
             id="bush"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x"
             alt=" "
           />
           <img
             id="w_rook_upright"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x"
             alt=" "
           />
         </div>
-        <div class="scenery" id="background1">
+        <div className="scenery" id="background1">
           <img
             id="r_pawn_upright"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x"
             alt=" "
           />
           <img
             id="w_rook"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x"
             alt=" "
           />
           <img
             id="palm1"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x"
             alt=" "
           />
         </div>
-        <div class="scenery" id="background2">
+        <div className="scenery" id="background2">
           <img
             id="r_pawn"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x"
             alt=" "
           />
 
           <img
             id="r_knight"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x"
             alt=" "
           />
           <img
             id="palm2"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x"
             alt=" "
           />
         </div>
